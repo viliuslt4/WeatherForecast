44d27adae116774f055ac8bd7750e0304e7dfaf4
viliuslt4 <viliuslt4@gmail.com>
2020-08-10 14:10:11 +0300

    Services created
    Api services created

--- src/app/forecast.service.ts
+++ src/app/forecast.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { ForecastRequest } from '../Models/forecast.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForecastService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getWeatherForecast(request: ForecastRequest){
+    return this.http.get<any>(environment.host + '/api/WeatherForecast/GetWeatherForecast?date='+ request.date +'&city='+ request.city);
+  }
+  public getListOfCities(){
+    return this.http.get<any>(environment.host + '/api/WeatherForecast/GetAllAvailableCities');
+  }
 }
